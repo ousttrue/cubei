75bae369abced5175cade1b18772a4f5ba70787a
ousttrue <ousttrue@gmail.com>
2023-01-19 02:24:40 +0900

    q3Vec3

--- qu3e/src/math/q3Vec3.cpp
+++ qu3e/src/math/q3Vec3.cpp
@@ -5,147 +5,111 @@
 @author	Randy Gaul
 @date	10/10/2014
 
-	Copyright (c) 2014 Randy Gaul http://www.randygaul.net
+        Copyright (c) 2014 Randy Gaul http://www.randygaul.net
 
-	This software is provided 'as-is', without any express or implied
-	warranty. In no event will the authors be held liable for any damages
-	arising from the use of this software.
+        This software is provided 'as-is', without any express or implied
+        warranty. In no event will the authors be held liable for any damages
+        arising from the use of this software.
 
-	Permission is granted to anyone to use this software for any purpose,
-	including commercial applications, and to alter it and redistribute it
-	freely, subject to the following restrictions:
-	  1. The origin of this software must not be misrepresented; you must not
-	     claim that you wrote the original software. If you use this software
-	     in a product, an acknowledgment in the product documentation would be
-	     appreciated but is not required.
-	  2. Altered source versions must be plainly marked as such, and must not
-	     be misrepresented as being the original software.
-	  3. This notice may not be removed or altered from any source distribution.
+        Permission is granted to anyone to use this software for any purpose,
+        including commercial applications, and to alter it and redistribute it
+        freely, subject to the following restrictions:
+          1. The origin of this software must not be misrepresented; you must
+not claim that you wrote the original software. If you use this software in a
+product, an acknowledgment in the product documentation would be appreciated but
+is not required.
+          2. Altered source versions must be plainly marked as such, and must
+not be misrepresented as being the original software.
+          3. This notice may not be removed or altered from any source
+distribution.
 */
 //--------------------------------------------------------------------------------------------------
 
-#include <cassert>
 #include "q3Vec3.h"
+#include <cassert>
 
 //--------------------------------------------------------------------------------------------------
 // q3Vec3
 //--------------------------------------------------------------------------------------------------
-q3Vec3::q3Vec3( )
-{
+q3Vec3::q3Vec3() {}
+
+//--------------------------------------------------------------------------------------------------
+q3Vec3::q3Vec3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
+
+//--------------------------------------------------------------------------------------------------
+void q3Vec3::Set(float _x, float _y, float _z) {
+  x = _x;
+  y = _y;
+  z = _z;
 }
 
 //--------------------------------------------------------------------------------------------------
-q3Vec3::q3Vec3( float _x, float _y, float _z )
-	: x( _x )
-	, y( _y )
-	, z( _z )
-{
+void q3Vec3::SetAll(float a) {
+  x = a;
+  y = a;
+  z = a;
 }
 
 //--------------------------------------------------------------------------------------------------
-void q3Vec3::Set( float _x, float _y, float _z )
-{
-	x = _x;
-	y = _y;
-	z = _z;
+q3Vec3 &q3Vec3::operator+=(const q3Vec3 &rhs) {
+  x += rhs.x;
+  y += rhs.y;
+  z += rhs.z;
+
+  return *this;
 }
 
 //--------------------------------------------------------------------------------------------------
-void q3Vec3::SetAll( float a )
-{
-	x = a;
-	y = a;
-	z = a;
+q3Vec3 &q3Vec3::operator-=(const q3Vec3 &rhs) {
+  x -= rhs.x;
+  y -= rhs.y;
+  z -= rhs.z;
+
+  return *this;
 }
 
 //--------------------------------------------------------------------------------------------------
-q3Vec3& q3Vec3::operator+=( const q3Vec3& rhs )
-{
-	x += rhs.x;
-	y += rhs.y;
-	z += rhs.z;
+q3Vec3 &q3Vec3::operator*=(float f) {
+  x *= f;
+  y *= f;
+  z *= f;
 
-	return *this;
+  return *this;
 }
 
 //--------------------------------------------------------------------------------------------------
-q3Vec3& q3Vec3::operator-=( const q3Vec3& rhs )
-{
-	x -= rhs.x;
-	y -= rhs.y;
-	z -= rhs.z;
+q3Vec3 &q3Vec3::operator/=(float f) {
+  x /= f;
+  y /= f;
+  z /= f;
 
-	return *this;
+  return *this;
 }
 
 //--------------------------------------------------------------------------------------------------
-q3Vec3& q3Vec3::operator*=( float f )
-{
-	x *= f;
-	y *= f;
-	z *= f;
+q3Vec3 q3Vec3::operator-(void) const { return q3Vec3(-x, -y, -z); }
 
-	return *this;
+//--------------------------------------------------------------------------------------------------
+const q3Vec3 q3Vec3::operator+(const q3Vec3 &rhs) const {
+  return q3Vec3(x + rhs.x, y + rhs.y, z + rhs.z);
 }
 
 //--------------------------------------------------------------------------------------------------
-q3Vec3& q3Vec3::operator/=( float f )
-{
-	x /= f;
-	y /= f;
-	z /= f;
-
-	return *this;
+const q3Vec3 q3Vec3::operator-(const q3Vec3 &rhs) const {
+  return q3Vec3(x - rhs.x, y - rhs.y, z - rhs.z);
 }
 
 //--------------------------------------------------------------------------------------------------
-float& q3Vec3::operator[]( uint32_t i )
-{
-	assert( i >= 0 && i < 3 );
-
-	return v[ i ];
+const q3Vec3 q3Vec3::operator*(float f) const {
+  return q3Vec3(x * f, y * f, z * f);
 }
 
 //--------------------------------------------------------------------------------------------------
-float q3Vec3::operator[]( uint32_t i ) const
-{
-	assert( i >= 0 && i < 3 );
-
-	return v[i];
+const q3Vec3 q3Vec3::operator/(float f) const {
+  return q3Vec3(x / f, y / f, z / f);
 }
 
 //--------------------------------------------------------------------------------------------------
-q3Vec3 q3Vec3::operator-( void ) const
-{
-	return q3Vec3( -x, -y, -z );
-}
-
-//--------------------------------------------------------------------------------------------------
-const q3Vec3 q3Vec3::operator+( const q3Vec3& rhs ) const
-{
-	return q3Vec3( x + rhs.x, y + rhs.y, z + rhs.z );
-}
-
-//--------------------------------------------------------------------------------------------------
-const q3Vec3 q3Vec3::operator-( const q3Vec3& rhs ) const
-{
-	return q3Vec3( x - rhs.x, y - rhs.y, z - rhs.z );
-}
-
-//--------------------------------------------------------------------------------------------------
-const q3Vec3 q3Vec3::operator*( float f ) const
-{
-	return q3Vec3( x * f, y * f, z * f );
-}
-
-//--------------------------------------------------------------------------------------------------
-const q3Vec3 q3Vec3::operator/( float f ) const
-{
-	return q3Vec3( x / f, y / f, z / f );
-}
-
-//--------------------------------------------------------------------------------------------------
-const q3Vec3 operator*( float f, const q3Vec3& rhs )
-{
-	return q3Vec3( rhs.x * f, rhs.y * f, rhs.z * f );
+const q3Vec3 operator*(float f, const q3Vec3 &rhs) {
+  return q3Vec3(rhs.x * f, rhs.y * f, rhs.z * f);
 }
--- qu3e/src/math/q3Vec3.h
+++ qu3e/src/math/q3Vec3.h
@@ -5,22 +5,23 @@
 @author	Randy Gaul
 @date	10/10/2014
 
-	Copyright (c) 2014 Randy Gaul http://www.randygaul.net
+        Copyright (c) 2014 Randy Gaul http://www.randygaul.net
 
-	This software is provided 'as-is', without any express or implied
-	warranty. In no event will the authors be held liable for any damages
-	arising from the use of this software.
+        This software is provided 'as-is', without any express or implied
+        warranty. In no event will the authors be held liable for any damages
+        arising from the use of this software.
 
-	Permission is granted to anyone to use this software for any purpose,
-	including commercial applications, and to alter it and redistribute it
-	freely, subject to the following restrictions:
-	  1. The origin of this software must not be misrepresented; you must not
-	     claim that you wrote the original software. If you use this software
-	     in a product, an acknowledgment in the product documentation would be
-	     appreciated but is not required.
-	  2. Altered source versions must be plainly marked as such, and must not
-	     be misrepresented as being the original software.
-	  3. This notice may not be removed or altered from any source distribution.
+        Permission is granted to anyone to use this software for any purpose,
+        including commercial applications, and to alter it and redistribute it
+        freely, subject to the following restrictions:
+          1. The origin of this software must not be misrepresented; you must
+not claim that you wrote the original software. If you use this software in a
+product, an acknowledgment in the product documentation would be appreciated but
+is not required.
+          2. Altered source versions must be plainly marked as such, and must
+not be misrepresented as being the original software.
+          3. This notice may not be removed or altered from any source
+distribution.
 */
 //--------------------------------------------------------------------------------------------------
 
@@ -28,47 +29,61 @@
 #define Q3VEC3_H
 
 #include "../common/q3Types.h"
+#include <stdexcept>
 
-float q3Abs( float a );
-float q3Min( float a, float b );
-float q3Max( float a, float b );
+float q3Abs(float a);
+float q3Min(float a, float b);
+float q3Max(float a, float b);
 
 //--------------------------------------------------------------------------------------------------
 // q3Vec3
 //--------------------------------------------------------------------------------------------------
-struct q3Vec3
-{
-	union
-	{
-		float v[ 3 ];
+struct q3Vec3 {
+  float x;
+  float y;
+  float z;
 
-		struct
-		{
-			float x;
-			float y;
-			float z;
-		};
-	};
+  q3Vec3();
+  q3Vec3(float _x, float _y, float _z);
 
-	q3Vec3( );
-	q3Vec3( float _x, float _y, float _z );
+  void Set(float _x, float _y, float _z);
+  void SetAll(float a);
+  q3Vec3 &operator+=(const q3Vec3 &rhs);
+  q3Vec3 &operator-=(const q3Vec3 &rhs);
+  q3Vec3 &operator*=(float f);
+  q3Vec3 &operator/=(float f);
 
-	void Set( float _x, float _y, float _z );
-	void SetAll( float a );
-	q3Vec3& operator+=( const q3Vec3& rhs );
-	q3Vec3& operator-=( const q3Vec3& rhs );
-	q3Vec3& operator*=( float f );
-	q3Vec3& operator/=( float f );
+  float &operator[](uint32_t i) {
+    switch (i) {
+    case 0:
+      return x;
+    case 1:
+      return y;
+    case 2:
+      return z;
+    default:
+      throw std::runtime_error("index out of range");
+    }
+  }
+  float operator[](uint32_t i) const {
+    switch (i) {
+    case 0:
+      return x;
+    case 1:
+      return y;
+    case 2:
+      return z;
+    default:
+      throw std::runtime_error("index out of range");
+    }
+  }
 
-	float& operator[]( uint32_t i );
-	float operator[]( uint32_t i ) const;
+  q3Vec3 operator-(void) const;
 
-	q3Vec3 operator-( void ) const;
-
-	const q3Vec3 operator+( const q3Vec3& rhs ) const;
-	const q3Vec3 operator-( const q3Vec3& rhs ) const;
-	const q3Vec3 operator*( float f ) const;
-	const q3Vec3 operator/( float f ) const;
+  const q3Vec3 operator+(const q3Vec3 &rhs) const;
+  const q3Vec3 operator-(const q3Vec3 &rhs) const;
+  const q3Vec3 operator*(float f) const;
+  const q3Vec3 operator/(float f) const;
 };
 
 #include "q3Vec3.inl"
