0c12f9e6b6d1cfc7121198a84f039b53e4a16a99
ousttrue <ousttrue@gmail.com>
2023-01-19 19:12:17 +0900

    span

--- qu3e/src/dynamics/q3ContactSolver.h
+++ qu3e/src/dynamics/q3ContactSolver.h
@@ -5,30 +5,32 @@
 @author	Randy Gaul
 @date	10/10/2014
 
-	Copyright (c) 2014 Randy Gaul http://www.randygaul.net
+        Copyright (c) 2014 Randy Gaul http://www.randygaul.net
 
-	This software is provided 'as-is', without any express or implied
-	warranty. In no event will the authors be held liable for any damages
-	arising from the use of this software.
+        This software is provided 'as-is', without any express or implied
+        warranty. In no event will the authors be held liable for any damages
+        arising from the use of this software.
 
-	Permission is granted to anyone to use this software for any purpose,
-	including commercial applications, and to alter it and redistribute it
-	freely, subject to the following restrictions:
-	  1. The origin of this software must not be misrepresented; you must not
-	     claim that you wrote the original software. If you use this software
-	     in a product, an acknowledgment in the product documentation would be
-	     appreciated but is not required.
-	  2. Altered source versions must be plainly marked as such, and must not
-	     be misrepresented as being the original software.
-	  3. This notice may not be removed or altered from any source distribution.
+        Permission is granted to anyone to use this software for any purpose,
+        including commercial applications, and to alter it and redistribute it
+        freely, subject to the following restrictions:
+          1. The origin of this software must not be misrepresented; you must
+not claim that you wrote the original software. If you use this software in a
+product, an acknowledgment in the product documentation would be appreciated but
+is not required.
+          2. Altered source versions must be plainly marked as such, and must
+not be misrepresented as being the original software.
+          3. This notice may not be removed or altered from any source
+distribution.
 */
 //--------------------------------------------------------------------------------------------------
 
 #ifndef Q3CONTACTSOLVER_H
 #define Q3CONTACTSOLVER_H
 
-#include "../math/q3Math.h"
 #include "../common/q3Settings.h"
+#include "../math/q3Math.h"
+#include <span>
 
 //--------------------------------------------------------------------------------------------------
 // q3ContactSolver
@@ -36,50 +38,51 @@
 struct q3Island;
 struct q3VelocityState;
 
-struct q3ContactState
-{
-	q3Vec3 ra;					// Vector from C.O.M to contact position
-	q3Vec3 rb;					// Vector from C.O.M to contact position
-	float penetration;			// Depth of penetration from collision
-	float normalImpulse;			// Accumulated normal impulse
-	float tangentImpulse[ 2 ];	// Accumulated friction impulse
-	float bias;					// Restitution + baumgarte
-	float normalMass;				// Normal constraint mass
-	float tangentMass[ 2 ];		// Tangent constraint mass
+struct q3ContactState {
+  q3Vec3 ra;               // Vector from C.O.M to contact position
+  q3Vec3 rb;               // Vector from C.O.M to contact position
+  float penetration;       // Depth of penetration from collision
+  float normalImpulse;     // Accumulated normal impulse
+  float tangentImpulse[2]; // Accumulated friction impulse
+  float bias;              // Restitution + baumgarte
+  float normalMass;        // Normal constraint mass
+  float tangentMass[2];    // Tangent constraint mass
 };
 
-struct q3ContactConstraintState
-{
-	q3ContactState contacts[ 8 ];
-	int contactCount;
-	q3Vec3 tangentVectors[ 2 ];	// Tangent vectors
-	q3Vec3 normal;				// From A to B
-	q3Vec3 centerA;
-	q3Vec3 centerB;
-	q3Mat3 iA;
-	q3Mat3 iB;
-	float mA;
-	float mB;
-	float restitution;
-	float friction;
-	int indexA;
-	int indexB;
+struct q3ContactConstraintState {
+  q3ContactState contacts[8];
+  int contactCount;
+  q3Vec3 tangentVectors[2]; // Tangent vectors
+  q3Vec3 normal;            // From A to B
+  q3Vec3 centerA;
+  q3Vec3 centerB;
+  q3Mat3 iA;
+  q3Mat3 iB;
+  float mA;
+  float mB;
+  float restitution;
+  float friction;
+  int indexA;
+  int indexB;
+
+  std::span<q3ContactState> span() {
+    return std::span(contacts, contactCount);
+  }
 };
 
-struct q3ContactSolver
-{
-	void Initialize( q3Island *island );
-	void ShutDown( void );
+struct q3ContactSolver {
+  void Initialize(q3Island *island);
+  void ShutDown(void);
 
-	void PreSolve( float dt );
-	void Solve( void );
+  void PreSolve(float dt);
+  void Solve(void);
 
-	q3Island *m_island;
-	q3ContactConstraintState *m_contacts;
-	int m_contactCount;
-	q3VelocityState *m_velocities;
+  q3Island *m_island;
+  q3ContactConstraintState *m_contacts;
+  int m_contactCount;
+  q3VelocityState *m_velocities;
 
-	bool m_enableFriction;
+  bool m_enableFriction;
 };
 
 #endif // Q3CONTACTSOLVER_H
--- qu3e/src/dynamics/q3Island.cpp
+++ qu3e/src/dynamics/q3Island.cpp
@@ -5,204 +5,195 @@
 @author	Randy Gaul
 @date	10/10/2014
 
-	Copyright (c) 2014 Randy Gaul http://www.randygaul.net
+        Copyright (c) 2014 Randy Gaul http://www.randygaul.net
 
-	This software is provided 'as-is', without any express or implied
-	warranty. In no event will the authors be held liable for any damages
-	arising from the use of this software.
+        This software is provided 'as-is', without any express or implied
+        warranty. In no event will the authors be held liable for any damages
+        arising from the use of this software.
 
-	Permission is granted to anyone to use this software for any purpose,
-	including commercial applications, and to alter it and redistribute it
-	freely, subject to the following restrictions:
-	  1. The origin of this software must not be misrepresented; you must not
-	     claim that you wrote the original software. If you use this software
-	     in a product, an acknowledgment in the product documentation would be
-	     appreciated but is not required.
-	  2. Altered source versions must be plainly marked as such, and must not
-	     be misrepresented as being the original software.
-	  3. This notice may not be removed or altered from any source distribution.
+        Permission is granted to anyone to use this software for any purpose,
+        including commercial applications, and to alter it and redistribute it
+        freely, subject to the following restrictions:
+          1. The origin of this software must not be misrepresented; you must
+not claim that you wrote the original software. If you use this software in a
+product, an acknowledgment in the product documentation would be appreciated but
+is not required.
+          2. Altered source versions must be plainly marked as such, and must
+not be misrepresented as being the original software.
+          3. This notice may not be removed or altered from any source
+distribution.
 */
 //--------------------------------------------------------------------------------------------------
 
 #include "q3Island.h"
-#include "q3Body.h"
-#include "../common/q3Memory.h"
-#include "q3ContactSolver.h"
-#include "../common/q3Settings.h"
 #include "../broadphase/q3BroadPhase.h"
+#include "../common/q3Memory.h"
+#include "../common/q3Settings.h"
+#include "q3Body.h"
 #include "q3Contact.h"
+#include "q3ContactSolver.h"
 
 #include <Remotery.h>
 
 //--------------------------------------------------------------------------------------------------
 // q3Island
 //--------------------------------------------------------------------------------------------------
-void q3Island::Solve( )
-{
-    rmt_ScopedCPUSample(q3IslandSolve, 0);
+void q3Island::Solve() {
+  rmt_ScopedCPUSample(q3IslandSolve, 0);
 
-	// Apply gravity
-	// Integrate velocities and create state buffers, calculate world inertia
-	for ( int i = 0 ; i < m_bodyCount; ++i )
-	{
-		q3Body *body = m_bodies[ i ];
-		q3VelocityState *v = m_velocities + i;
+  // Apply gravity
+  // Integrate velocities and create state buffers, calculate world inertia
+  for (int i = 0; i < m_bodyCount; ++i) {
+    q3Body *body = m_bodies[i];
+    q3VelocityState *v = m_velocities + i;
 
-		if ( body->m_flags & q3Body::eDynamic )
-		{
-			body->ApplyLinearForce( m_gravity * body->m_gravityScale );
+    if (body->m_flags & q3Body::eDynamic) {
+      body->ApplyLinearForce(m_gravity * body->m_gravityScale);
 
-			// Calculate world space intertia tensor
-			q3Mat3 r = body->m_tx.rotation;
-			body->m_invInertiaWorld = r * body->m_invInertiaModel * q3Transpose( r );
+      // Calculate world space intertia tensor
+      q3Mat3 r = body->m_tx.rotation;
+      body->m_invInertiaWorld = r * body->m_invInertiaModel * q3Transpose(r);
 
-			// Integrate velocity
-			body->m_linearVelocity += (body->m_force * body->m_invMass) * m_dt;
-			body->m_angularVelocity += (body->m_invInertiaWorld * body->m_torque) * m_dt;
+      // Integrate velocity
+      body->m_linearVelocity += (body->m_force * body->m_invMass) * m_dt;
+      body->m_angularVelocity +=
+          (body->m_invInertiaWorld * body->m_torque) * m_dt;
 
-			// From Box2D!
-			// Apply damping.
-			// ODE: dv/dt + c * v = 0
-			// Solution: v(t) = v0 * exp(-c * t)
-			// Time step: v(t + dt) = v0 * exp(-c * (t + dt)) = v0 * exp(-c * t) * exp(-c * dt) = v * exp(-c * dt)
-			// v2 = exp(-c * dt) * v1
-			// Pade approximation:
-			// v2 = v1 * 1 / (1 + c * dt)
-			body->m_linearVelocity *= float( 1.0 ) / (float( 1.0 ) + m_dt * body->m_linearDamping);
-			body->m_angularVelocity *= float( 1.0 ) / (float( 1.0 ) + m_dt * body->m_angularDamping);
-		}
+      // From Box2D!
+      // Apply damping.
+      // ODE: dv/dt + c * v = 0
+      // Solution: v(t) = v0 * exp(-c * t)
+      // Time step: v(t + dt) = v0 * exp(-c * (t + dt)) = v0 * exp(-c * t) *
+      // exp(-c * dt) = v * exp(-c * dt) v2 = exp(-c * dt) * v1 Pade
+      // approximation: v2 = v1 * 1 / (1 + c * dt)
+      body->m_linearVelocity *=
+          float(1.0) / (float(1.0) + m_dt * body->m_linearDamping);
+      body->m_angularVelocity *=
+          float(1.0) / (float(1.0) + m_dt * body->m_angularDamping);
+    }
 
-		v->v = body->m_linearVelocity;
-		v->w = body->m_angularVelocity;
-	}
+    v->v = body->m_linearVelocity;
+    v->w = body->m_angularVelocity;
+  }
 
-	// Create contact solver, pass in state buffers, create buffers for contacts
-	// Initialize velocity constraint for normal + friction and warm start
-	q3ContactSolver contactSolver;
-	contactSolver.Initialize( this );
-	contactSolver.PreSolve( m_dt );
+  // Create contact solver, pass in state buffers, create buffers for contacts
+  // Initialize velocity constraint for normal + friction and warm start
+  q3ContactSolver contactSolver;
+  contactSolver.Initialize(this);
+  contactSolver.PreSolve(m_dt);
 
-	// Solve contacts
-	for ( int i = 0; i < m_iterations; ++i )
-		contactSolver.Solve( );
+  // Solve contacts
+  for (int i = 0; i < m_iterations; ++i)
+    contactSolver.Solve();
 
-	contactSolver.ShutDown( );
+  contactSolver.ShutDown();
 
-	// Copy back state buffers
-	// Integrate positions
-	for ( int i = 0 ; i < m_bodyCount; ++i )
-	{
-		q3Body *body = m_bodies[ i ];
-		q3VelocityState *v = m_velocities + i;
+  // Copy back state buffers
+  // Integrate positions
+  for (int i = 0; i < m_bodyCount; ++i) {
+    q3Body *body = m_bodies[i];
+    q3VelocityState *v = m_velocities + i;
 
-		if ( body->m_flags & q3Body::eStatic )
-			continue;
+    if (body->m_flags & q3Body::eStatic)
+      continue;
 
-		body->m_linearVelocity = v->v;
-		body->m_angularVelocity = v->w;
+    body->m_linearVelocity = v->v;
+    body->m_angularVelocity = v->w;
 
-		// Integrate position
-		body->m_worldCenter += body->m_linearVelocity * m_dt;
-		body->m_q.Integrate( body->m_angularVelocity, m_dt );
-		body->m_q = q3Normalize( body->m_q );
-		body->m_tx.rotation = body->m_q.ToMat3( );
-	}
+    // Integrate position
+    body->m_worldCenter += body->m_linearVelocity * m_dt;
+    body->m_q.Integrate(body->m_angularVelocity, m_dt);
+    body->m_q = q3Normalize(body->m_q);
+    body->m_tx.rotation = body->m_q.ToMat3();
+  }
 
-	if ( m_allowSleep )
-	{
-		// Find minimum sleep time of the entire island
-		float minSleepTime = Q3_R32_MAX;
-		for ( int i = 0; i < m_bodyCount; ++i )
-		{
-			q3Body* body = m_bodies[ i ];
+  if (m_allowSleep) {
+    // Find minimum sleep time of the entire island
+    float minSleepTime = Q3_R32_MAX;
+    for (int i = 0; i < m_bodyCount; ++i) {
+      q3Body *body = m_bodies[i];
 
-			if ( body->m_flags & q3Body::eStatic )
-				continue;
+      if (body->m_flags & q3Body::eStatic)
+        continue;
 
-			const float sqrLinVel = q3Dot( body->m_linearVelocity, body->m_linearVelocity );
-			const float cbAngVel = q3Dot( body->m_angularVelocity, body->m_angularVelocity );
-			const float linTol = Q3_SLEEP_LINEAR;
-			const float angTol = Q3_SLEEP_ANGULAR;
+      const float sqrLinVel =
+          q3Dot(body->m_linearVelocity, body->m_linearVelocity);
+      const float cbAngVel =
+          q3Dot(body->m_angularVelocity, body->m_angularVelocity);
+      const float linTol = Q3_SLEEP_LINEAR;
+      const float angTol = Q3_SLEEP_ANGULAR;
 
-			if ( sqrLinVel > linTol || cbAngVel > angTol )
-			{
-				minSleepTime = float( 0.0 );
-				body->m_sleepTime = float( 0.0 );
-			}
+      if (sqrLinVel > linTol || cbAngVel > angTol) {
+        minSleepTime = float(0.0);
+        body->m_sleepTime = float(0.0);
+      }
 
-			else
-			{
-				body->m_sleepTime += m_dt;
-				minSleepTime = q3Min( minSleepTime, body->m_sleepTime );
-			}
-		}
+      else {
+        body->m_sleepTime += m_dt;
+        minSleepTime = q3Min(minSleepTime, body->m_sleepTime);
+      }
+    }
 
-		// Put entire island to sleep so long as the minimum found sleep time
-		// is below the threshold. If the minimum sleep time reaches below the
-		// sleeping threshold, the entire island will be reformed next step
-		// and sleep test will be tried again.
-		if ( minSleepTime > Q3_SLEEP_TIME )
-		{
-			for ( int i = 0; i < m_bodyCount; ++i )
-				m_bodies[ i ]->SetToSleep( );
-		}
-	}
+    // Put entire island to sleep so long as the minimum found sleep time
+    // is below the threshold. If the minimum sleep time reaches below the
+    // sleeping threshold, the entire island will be reformed next step
+    // and sleep test will be tried again.
+    if (minSleepTime > Q3_SLEEP_TIME) {
+      for (int i = 0; i < m_bodyCount; ++i)
+        m_bodies[i]->SetToSleep();
+    }
+  }
 }
 
 //--------------------------------------------------------------------------------------------------
-void q3Island::Add( q3Body *body )
-{
-	assert( m_bodyCount < m_bodyCapacity );
+void q3Island::Add(q3Body *body) {
+  assert(m_bodyCount < m_bodyCapacity);
 
-	body->m_islandIndex = m_bodyCount;
+  body->m_islandIndex = m_bodyCount;
 
-	m_bodies[ m_bodyCount++ ] = body;
+  m_bodies[m_bodyCount++] = body;
 }
 
 //--------------------------------------------------------------------------------------------------
-void q3Island::Add( q3ContactConstraint *contact )
-{
-	assert( m_contactCount < m_contactCapacity );
+void q3Island::Add(q3ContactConstraint *contact) {
+  assert(m_contactCount < m_contactCapacity);
 
-	m_contacts[ m_contactCount++ ] = contact;
+  m_contacts[m_contactCount++] = contact;
 }
 
 //--------------------------------------------------------------------------------------------------
-void q3Island::Initialize( )
-{
-    rmt_ScopedCPUSample(q3IslandInitialize, 0);
+void q3Island::Initialize() {
+  rmt_ScopedCPUSample(q3IslandInitialize, 0);
 
-	for ( int i = 0; i < m_contactCount; ++i )
-	{
-		q3ContactConstraint *cc = m_contacts[ i ];
+  for (int i = 0; i < m_contactCount; ++i) {
+    q3ContactConstraint *cc = m_contacts[i];
 
-		q3ContactConstraintState *c = m_contactStates + i;
+    q3ContactConstraintState *c = m_contactStates + i;
 
-		c->centerA = cc->bodyA->m_worldCenter;
-		c->centerB = cc->bodyB->m_worldCenter;
-		c->iA = cc->bodyA->m_invInertiaWorld;
-		c->iB = cc->bodyB->m_invInertiaWorld;
-		c->mA = cc->bodyA->m_invMass;
-		c->mB = cc->bodyB->m_invMass;
-		c->restitution = cc->restitution;
-		c->friction = cc->friction;
-		c->indexA = cc->bodyA->m_islandIndex;
-		c->indexB = cc->bodyB->m_islandIndex;
-		c->normal = cc->manifold.normal;
-		c->tangentVectors[ 0 ] = cc->manifold.tangentVectors[ 0 ];
-		c->tangentVectors[ 1 ] = cc->manifold.tangentVectors[ 1 ];
-		c->contactCount = cc->manifold.contactCount;
+    c->centerA = cc->bodyA->m_worldCenter;
+    c->centerB = cc->bodyB->m_worldCenter;
+    c->iA = cc->bodyA->m_invInertiaWorld;
+    c->iB = cc->bodyB->m_invInertiaWorld;
+    c->mA = cc->bodyA->m_invMass;
+    c->mB = cc->bodyB->m_invMass;
+    c->restitution = cc->restitution;
+    c->friction = cc->friction;
+    c->indexA = cc->bodyA->m_islandIndex;
+    c->indexB = cc->bodyB->m_islandIndex;
+    c->normal = cc->manifold.normal;
+    c->tangentVectors[0] = cc->manifold.tangentVectors[0];
+    c->tangentVectors[1] = cc->manifold.tangentVectors[1];
+    c->contactCount = cc->manifold.contactCount;
 
-		for ( int j = 0; j < c->contactCount; ++j )
-		{
-			q3ContactState *s = c->contacts + j;
-			q3Contact *cp = cc->manifold.contacts + j;
-			s->ra = cp->position - c->centerA;
-			s->rb = cp->position - c->centerB;
-			s->penetration = cp->penetration;
-			s->normalImpulse = cp->normalImpulse;
-			s->tangentImpulse[ 0 ] = cp->tangentImpulse[ 0 ];
-			s->tangentImpulse[ 1 ] = cp->tangentImpulse[ 1 ];
-		}
-	}
+    int j = 0;
+    for (auto &s : c->span()) {
+      auto cp = &cc->manifold.contacts[j++];
+      s.ra = cp->position - c->centerA;
+      s.rb = cp->position - c->centerB;
+      s.penetration = cp->penetration;
+      s.normalImpulse = cp->normalImpulse;
+      s.tangentImpulse[0] = cp->tangentImpulse[0];
+      s.tangentImpulse[1] = cp->tangentImpulse[1];
+    }
+  }
 }
