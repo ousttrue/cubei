0da9448a00465fc12f46357a1418a9db15e8b152
ousttrue <ousttrue@gmail.com>
2023-01-19 13:53:17 +0900

    Test

--- qu3e/demo/demos/Test.cpp
+++ qu3e/demo/demos/Test.cpp
@@ -1,22 +1,35 @@
 #include "Test.h"
 
 void Test::Init(q3Scene *scene) {
-  q3BodyDef bodyDef;
-  q3Body *body = scene->CreateBody(bodyDef);
-  q3BoxDef boxDef{
-      .m_restitution = 0,
-  };
-  q3Transform tx = {};
-  boxDef.Set(tx, {50.0f, 1.0f, 50.0f});
-  body->AddBox(boxDef);
+  {
+    auto body = scene->CreateBody({});
+    body->AddBox({
+        .m_tx = {},
+        .m_e = {50.0f, 1.0f, 50.0f},
+        .m_restitution = 0,
+    });
+  }
 
-  bodyDef.bodyType = eDynamicBody;
-  bodyDef.position.Set(0, 5.0f, 0);
-  body = scene->CreateBody(bodyDef);
-  for (int i = 0; i < 20; ++i) {
-    tx.position.Set(q3RandomFloat(1.0f, 10.0f), q3RandomFloat(1.0f, 10.0f),
-                    q3RandomFloat(1.0f, 10.0f));
-    boxDef.Set(tx, {1.0f, 1.0f, 1.0f});
-    body->AddBox(boxDef);
+  {
+    auto body = scene->CreateBody({
+        .position = {0, 5.0f, 0},
+        .bodyType = eDynamicBody,
+    });
+    for (int i = 0; i < 20; ++i) {
+      q3BoxDef boxDef{
+          .m_tx =
+              {
+                  .position =
+                      {
+                          q3RandomFloat(1.0f, 10.0f),
+                          q3RandomFloat(1.0f, 10.0f),
+                          q3RandomFloat(1.0f, 10.0f),
+                      },
+              },
+          .m_e = {1.0f, 1.0f, 1.0f},
+          .m_restitution = 0,
+      };
+      body->AddBox(boxDef);
+    }
   }
 }
